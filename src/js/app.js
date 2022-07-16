f46b092c5a127b27ac982aa1aa65a78c7ac4cac4
SERHII <dmitroprojec89@gmail.com>
2022-07-16 12:05:05 +0300

    registration1607

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,25 @@
-console.log('hello');
+// console.log('hello');
 
-import * as flsFunctions from "./modules/functions.js";
+// import * as flsFunctions from "./modules/functions.js";
 
-flsFunctions.isWebp();
+// flsFunctions.isWebp();
 
-import Swiper, {Navigator, Pagination} from 'swiper';
+// import Swiper, {Navigator, Pagination} from 'swiper';
 
-const swiper = new Swiper();
+// const swiper = new Swiper();
+
+'use strict';
+
+let wrap = document.querySelector('.wrapper');
+
+console.log(wrap);
+
+wrap.addEventListener('click',(event) => {
+    let t = event.target;
+    console.log(t);
+    if(t.matches('.btn')) {
+        wrap.querySelector('.wrapper__bg').classList.toggle('wrapper__bg--active');
+        wrap.querySelector('.container1').classList.toggle('container1--active');
+        wrap.querySelector('.container2').classList.toggle('container2--active');
+    }
+});
